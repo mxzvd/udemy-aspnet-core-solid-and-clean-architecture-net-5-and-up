812633d09a260ceb56849b4fdb0a837a3547b764
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2022-08-24 00:21:23 +0200

    Implemented contracts and interfaces for the application core layer

---
+++ src/Core/HR.LeaveManagement.Application/Persistence/Contracts/ILeaveRequestRepository.cs
@@ -0,0 +1,7 @@
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.Persistence.Contracts;
+
+public interface ILeaveRequestRepository : IGenericRepository<LeaveRequest>
+{
+}
---
+++ src/Core/HR.LeaveManagement.Application/Persistence/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,7 @@
+using HR.LeaveManagement.Domain;
+
+namespace HR.LeaveManagement.Application.Persistence.Contracts;
+
+public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
+{
+}
