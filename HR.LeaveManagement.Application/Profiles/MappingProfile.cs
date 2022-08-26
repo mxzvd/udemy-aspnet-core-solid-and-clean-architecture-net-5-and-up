d52eff5baaa3a68beffe25399b48a68b47ac9673
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2022-08-26 23:54:31 +0200

    Improved the mappings

--- HR.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -10,16 +10,22 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
+        #region LeaveRequest Mappings
         CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
         CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
         CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
         CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+        #endregion LeaveRequest
 
+        #region LeaveAllocation Mappings
         CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
         CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
         CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
+        #endregion LeaveAllocation
 
+        #region LeaveType Mappings
         CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
         CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+        #endregion LeaveType
     }
 }
