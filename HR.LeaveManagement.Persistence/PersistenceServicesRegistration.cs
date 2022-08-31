154d7895d885c4c13214e6584900a13356363e95
Maxim-Zavidei <maxim-zavidei+github@pm.me>
2022-08-31 13:11:19 +0200

    Implemented the registration service for the persistence

---
+++ HR.LeaveManagement.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,24 @@
+using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HR.LeaveManagement.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<LeaveManagementDbContext>(options => options.UseNpgsql(
+            configuration.GetConnectionString("DefaultConnection")
+        ));
+
+        services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        services.AddScoped<ILeaveTypeRepository, LeaveTypeRepository>();
+        services.AddScoped<ILeaveRequestRepository, LeaveRequestRepository>();
+        services.AddScoped<ILeaveAllocationRepository, LeaveAllocationRepository>();
+
+        return services;
+    }
+}
